a2d7d8f69bc1a13e04e5b5d87d2c87640c510409
taifoor-iq <iqbaltaifoor19@gmail.com>
2026-05-15 15:05:14 +0500

    Added basic calculator file

--- Week 09/Basic_Calculator.py
+++ Week 09/Basic_Calculator.py
@@ -19,30 +19,85 @@ def calculate(operation):
     except Exception as e:
         result_label.config(text="Error")
 
+
+# Clear Function
+def clear():
+    entry1.delete(0, tk.END)
+    entry2.delete(0, tk.END)
+    result_label.config(text="Result: ")
+
+
 # Main Window
 root = tk.Tk()
 root.title("Simple Calculator")
-root.geometry("300x300")
+root.geometry("300x350")
+root.config(bg="lightblue")
 
-#Widgets
+
+# Widgets
 
 # Inputs
-tk.Label(root, text="Enter first number").pack()
+tk.Label(root, text="Enter first number", bg="lightblue").pack()
+
 entry1 = tk.Entry(root)
 entry1.pack()
 
-tk.Label(root, text="Enter second number").pack()
+tk.Label(root, text="Enter second number", bg="lightblue").pack()
+
 entry2 = tk.Entry(root)
 entry2.pack()
 
+
 # Buttons
-tk.Button(root, text="Add", command=lambda: calculate("add")).pack(pady=5)
-tk.Button(root, text="Subtract", command=lambda: calculate("sub")).pack(pady=5)
-tk.Button(root, text="Multiply", command=lambda: calculate("mul")).pack(pady=5)
-tk.Button(root, text="Divide", command=lambda: calculate("div")).pack(pady=5)
+tk.Button(
+    root,
+    text="Add",
+    bg="green",
+    fg="white",
+    command=lambda: calculate("add")
+).pack(pady=5)
+
+tk.Button(
+    root,
+    text="Subtract",
+    bg="orange",
+    fg="white",
+    command=lambda: calculate("sub")
+).pack(pady=5)
+
+tk.Button(
+    root,
+    text="Multiply",
+    bg="blue",
+    fg="white",
+    command=lambda: calculate("mul")
+).pack(pady=5)
+
+tk.Button(
+    root,
+    text="Divide",
+    bg="purple",
+    fg="white",
+    command=lambda: calculate("div")
+).pack(pady=5)
+
+# Clear Button
+tk.Button(
+    root,
+    text="Clear",
+    bg="red",
+    fg="white",
+    command=clear
+).pack(pady=5)
+
 
 # Result
-result_label = tk.Label(root, text="Result: ")
+result_label = tk.Label(
+    root,
+    text="Result: ",
+    bg="lightblue"
+)
+
 result_label.pack(pady=10)
 
 root.mainloop()
--- Week 09/Calculator.py
+++ Week 09/Calculator.py
@@ -1,3 +1,4 @@
+
 import tkinter as tk
 
 def press(key):
